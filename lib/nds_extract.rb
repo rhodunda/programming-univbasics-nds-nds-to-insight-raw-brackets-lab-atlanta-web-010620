859f8b6fc59a5016aeac4d6e93cdbcd81548c9fb
Bryan Rhodunda <rhodunda@udel.edu>
2019-12-24 04:10:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,7 +12,8 @@ while directors_totals < nds.lenght do
   movie_index = 0 
   
   
-  while movie_index < nds[director_index][:movies].lenght
+  while movie_index < nds[director_index][:movies].lenght do
+    
   
   
   
